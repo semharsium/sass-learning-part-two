e8ce010ce7e069ca182899c06ff9c9cbb7f759f4
Semhar Sium <semhar50@gmail.com>
2019-01-29 16:28:55 +0100

    class name changed

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -2,14 +2,14 @@ $( document ).ready(function() {
 
     $( ".cross" ).hide();
     $( ".hamburger" ).click(function() {
-        $( ".js-navigation__menu" ).slideToggle( "slow", function() {
+        $( ".js-navigation__item-container" ).slideToggle( "slow", function() {
             $( ".hamburger" ).hide();
             $( ".cross" ).show();
         });
     });
         
     $( ".cross" ).click(function() {
-        $( ".js-navigation__menu" ).slideToggle( "slow", function() {
+        $( ".js-navigation__item-container" ).slideToggle( "slow", function() {
             $( ".cross" ).hide();
             $( ".hamburger" ).show();
         });
